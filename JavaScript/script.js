b9ade3b86ca7b5186fcdc5b30281b40772966293
Janete <janeteleiviane@gmail.com>
2022-11-15 22:49:25 -0300

    Entregaveis do curso HTML

---
+++ JavaScript/script.js
@@ -0,0 +1,46 @@
+//comentário em linha
+/*
+comentário em bloco
+
+*/
+
+/*console.log("olha eu no 'aspas' simples!! ")
+
+console.log('olha eu no "aspas" simples!! ')
+
+console.log(`ol'ha 'eu' 'no'
+ "aspas" simples'!! `)
+ */
+
+/*console.log(12) //Integer
+console.log(24.5) //Float
+console.log(12*24/4) //calc
+console.log(Infinity)
+console.log(50+(53*4.5))
+console.log(Math.round(50+(53*4.5)))
+*/
+
+/*console.log(42 == 32) (true or false)*/
+
+//console.log(null == undefined)
+
+ //Objeto
+/* console.log({
+    marca: 'Fiat',
+    largura: '550cm',
+    peso: 1,
+    velocidade: (90 * 200) *2,
+    andar: function (){
+        console.log(`Sua velocidade é de 200 `)
+    },
+
+ })*/
+
+ //Array
+ console.log([
+    "Bolacha",
+    "Coca-cola",
+    "Verdura",
+    "Água"
+
+ ])
